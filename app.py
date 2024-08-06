fb2a3163ddeedf90478565995ca5190c666d8796
srikargupta1144 <srikartadakamalla22@gmail.com>
2024-08-06 18:48:21 +0530

    commit

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-# we are using language_tool_python for this project, it except 500 words
-
-# first we take input from user and detect grammar & spelling mistakes, then we will use the output of language_tool_python to correct the mistakes
-
 import streamlit as st
-import language_tool_python 
+import language_tool_python
 
-# text correcting functions
+# Text correcting function
 def text_correction(text):
-    tool = language_tool_python.LanguageTool('en-US')
-    matches = tool.check(text)
-    corrected_text = language_tool_python.utils.correct(text, matches)
-    return corrected_text
+    try:
+        tool = language_tool_python.LanguageTool('en-US')
+        matches = tool.check(text)
+        corrected_text = language_tool_python.utils.correct(text, matches)
+        return corrected_text
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
 
 def main():
     st.header('Spelling & Grammar Mistakes Correction App')
